b54eb576ffa8290654bfde78135f283f426827d8
Adhilkv <imdhilu@gmail.com>
2022-10-08 22:20:01 +0530

    2nd commit

---
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -0,0 +1,34 @@
+package com.in28minutes.springboot.myfirstwebapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+
+	@RequestMapping("say-hello")
+	@ResponseBody
+	public String SayHello() {
+		return "Hello! What are you learning today";
+	}
+
+	@RequestMapping("say-hello-html")
+	@ResponseBody
+	public String SayHelloHtml() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("<html>");
+		sb.append("<head>");
+		sb.append("<title>");
+		sb.append("Html Page");
+		sb.append("</title>");
+		sb.append("</head>");
+		sb.append("<body>");
+		sb.append("My First Html Page....");
+		sb.append("</body>");
+		sb.append("</html>");
+		
+		return sb.toString();
+	}
+
+}
