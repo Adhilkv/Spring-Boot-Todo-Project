b29ea33e63d85564f43de0cc5b4f038253fbae20
Adhilkv <imdhilu@gmail.com>
2022-10-10 21:14:26 +0530

    3st jsp commit

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -2,12 +2,14 @@ package com.in28minutes.springboot.myfirstwebapp.login;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
 	@RequestMapping("login")
-	public String gotoLoginPage() {
+	public String gotoLoginPage(@RequestParam String name) {
+		System.out.println("Name : " + name);
 		return "login";
 	}
 
